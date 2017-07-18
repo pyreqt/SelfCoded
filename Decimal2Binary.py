8f5dd01e9086730587348a5b46ccf2f33e3e2409
pyreqt <thisiskmanoj@gmail.com>
2017-07-18 13:22:20 +0530

    Initial commit

---
+++ Decimal2Binary.py
@@ -0,0 +1,20 @@
+
+def binary(n):
+
+      if n==1:
+        a.append(n)
+        return
+
+      elif (n % 2 in (0, 1)):
+        a.append(n%2)
+        n_nxt=int(n/2)
+        binary(n_nxt)
+
+a=[]
+items=[int(x) for x in input("Enter the test cases: ").split(' ')]     #
+for i in (items):
+  binary(i)
+  string = ''.join(str(e) for e in a[::-1])  # convert list to string using join, if list has string just ''.join(list)
+  print(i,':',string)                              #list slicing to print in reverse order
+  a.clear()                                   # for the next number to be fed into the list
+
